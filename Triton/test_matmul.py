aadd29294ab5473ec9377fafb5ff1ac0a987728a
Castorp Winston <xvyv99@163.com>
2024-11-16 19:41:35 +0800

    modified:   Triton/test_matmul.py

--- Triton/test_matmul.py
+++ Triton/test_matmul.py
@@ -7,23 +7,23 @@ from matmul import matmul_triton, matmul_scalar
 from util import matmul_verify
 from tqdm import trange
 
-TRY_NUM = 10
+TRY_NUM = 128
 MNK_RANGE_MIN = 16 # 由于 tl.dot 会要求矩阵的行数和列数不小于16, 故添加此约束
-MNK_RANGE_MAX = 4096
+MNK_RANGE_MAX = 1024
 
 def test_matmul_tirton():
-    for t in trange(TRY_NUM):
+    for t in range(TRY_NUM):
         M, K, N = torch.randint(MNK_RANGE_MIN, MNK_RANGE_MAX, (3,))
-        A = torch.randn((M, K), device='cuda')
-        B = torch.randn((K, N), device='cuda')
+        A = torch.randn((M, K), device='cuda', dtype=torch.float32)
+        B = torch.randn((K, N), device='cuda', dtype=torch.float32)
         res = matmul_triton(A, B)
         matmul_verify(A, B, res)
 
 # 由于我写的标量矩阵乘法太慢了, 故缩小迭代范围
-TRY_NUM = 10
-MNK_RANGE_MIN = 16
-MNK_RANGE_MAX = 32
 def test_matmul_scalar():
+    TRY_NUM = 10
+    MNK_RANGE_MIN = 16
+    MNK_RANGE_MAX = 32
     for t in trange(TRY_NUM):
         M, K, N = torch.randint(MNK_RANGE_MIN, MNK_RANGE_MAX, (3,))
         A = torch.randn((M, K))
